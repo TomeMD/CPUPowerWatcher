bc4cad7b6ab7411c8f937533f2f9344948db3983
TomeMD <tome.maseda@udc.es>
2023-05-06 16:25:53 +0200

    Remove useless sleep

--- bin/init.sh
+++ bin/init.sh
@@ -17,11 +17,9 @@ mkdir -p $LOG_DIR
 # Start monitoring environment
 if [ "$USE_DOCKER" -eq "0" ]; then
 	docker run -d --name glances --pid host --privileged --network host --restart=unless-stopped -e GLANCES_OPT="-q --export influxdb2 --time 2" -v $(pwd)/${GLANCES_HOME}/etc/glances.conf:/glances/conf/glances.conf glances
-	sleep 6
 	docker run -d --name rapl --pid host --privileged --network host --restart=unless-stopped rapl
 else
 	sudo apptainer instance start --env "GLANCES_OPT=-q --export influxdb2 --time 2" --bind ${GLANCES_HOME}/etc/glances.conf:/glances/conf/glances.conf ${GLANCES_HOME}/glances.sif glances
-	sleep 6
 	sudo apptainer instance start ${RAPL_HOME}/rapl.sif rapl
 fi
 
