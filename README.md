4d8af5dfaabccdebe54dba9ea3aab599c30bf362
TomeMD <tome.maseda@udc.es>
2023-09-08 11:05:38 +0200

    Modified options parsing

--- README.md
+++ README.md
@@ -6,14 +6,20 @@ Set of scripts to automate CPU monitoring and time series storage while running 
 ~$ ./run.sh --help
 
 Usage: run.sh [OPTIONS]
+  -v, --os-virt             Technology for OS-level virtualization. [Default]
+                                docker
+                                apptainer
 
-Options:
-  -d, --docker             Use Docker for OS-level virtualization. [Default]
-  -a, --apptainer          Use Apptainer for OS-level virtualization.
-  -s, -stress-tests        Run stress tests using stress-system tool. [Default]
-  -n, --npb                Run NPB kenerls.
-  -g, --geekbench          Run Geekbench kenerls.
-  -o, --output <dir>       Directory to store log files. [Default: ./log]      
+  -w, --workload            Workload to stress the system with. [Default: stress-system]
+                              npb                 Run NPB kenerls.
+                              geekbench           Run Geekbench kenerls.
+                              stress-system       Run stress tests using stress-system tool. Options:
+                                --stressors              Comma-separated list of stressors to run with stress-system.
+                                                         [Default: cpu]
+                                --stress-load-types      Comma-separated list of types of load to stress the CPU.
+                                                         Used together with CPU stressor. [Default: all]
+
+  -o, --output <dir>       Directory (absolute path) to store log files. [Default: ./log]
   -h, --help               Show this help and exit
 ````
 
--- bin/parse-arguments.sh
+++ bin/parse-arguments.sh
@@ -3,19 +3,19 @@
 function usage {
   cat << EOF
 Usage: $(basename "$0") [OPTIONS]
+  -v, --os-virt             Technology for OS-level virtualization. [Default]
+                                docker
+                                apptainer
 
-OS-LEVEL VIRTUALIZATION:
-  -d, --docker             Use Docker for OS-level virtualization. [Default]
-  -a, --apptainer          Use Apptainer for OS-level virtualization.
+  -w, --workload            Workload to stress the system with. [Default: stress-system]
+                              npb                 Run NPB kenerls.
+                              geekbench           Run Geekbench kenerls.
+                              stress-system       Run stress tests using stress-system tool. Options:
+                                --stressors              Comma-separated list of stressors to run with stress-system.
+                                                         [Default: cpu]
+                                --stress-load-types      Comma-separated list of types of load to stress the CPU.
+                                                         Used together with CPU stressor. [Default: all]
 
-WORKLOAD:
-  -s, -stress-system       Run stress tests using stress-system tool. [Default]
-      --stressors              Comma-separated list of stressors to run with stress-system. [Default: cpu]
-      --stress-load-types      Comma-separated list of types of load to stress the CPU. Used together with CPU stressor. [Default: all]
-  -n, --npb                Run NPB kenerls.
-  -g, --geekbench          Run Geekbench kenerls.
-
-GLOBAL OPTIONS:
   -o, --output <dir>       Directory (absolute path) to store log files. [Default: ./log]
   -h, --help               Show this help and exit
 EOF
@@ -24,21 +24,13 @@ exit 1
 
 while [[ $# -gt 0 ]]; do
   case $1 in
-    -o|--output)
-      LOG_DIR="$2"
+    -v|--os-virt)
+      OS_VIRT="$2"
       shift 2
       ;;
-    -d|--docker)
-      OS_VIRT="docker"
-      shift 1
-      ;;
-    -a|--apptainer)
-      OS_VIRT="apptainer"
-      shift 1
-      ;;
-    -s|--stress-tests)
-      WORKLOAD="stress-system"
-      shift 1
+    -w|--workload)
+      WORKLOAD="$2"
+      shift 2
       ;;
     --stressors)
       STRESSORS="$2"
@@ -48,13 +40,9 @@ while [[ $# -gt 0 ]]; do
       LOAD_TYPES="$2"
       shift 2
       ;;
-    -n|--npb)
-      WORKLOAD="npb"
-      shift 1
-      ;;
-    -g|--geekbench)
-      WORKLOAD="geekbench"
-      shift 1
+    -o|--output)
+      LOG_DIR="$2"
+      shift 2
       ;;
     -h|--help)
       usage
