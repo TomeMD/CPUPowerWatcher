7c440a532fc6ffbcc9fe3dd73effdb6626689af8
TomeMD <tome.maseda@udc.es>
2023-10-24 21:56:57 +0200

    Minor fixes

--- bin/init.sh
+++ bin/init.sh
@@ -5,7 +5,7 @@ if [ "$OS_VIRT" == "docker" ]; then
 	docker run -d --name glances --pid host --privileged --network host --restart=unless-stopped -e GLANCES_OPT="-q --export influxdb2 --time 2" glances "${INFLUXDB_HOST}" "${INFLUXDB_BUCKET}"
 	docker run -d --name rapl --pid host --privileged --network host --restart=unless-stopped rapl "${INFLUXDB_HOST}" "${INFLUXDB_BUCKET}"
 else
-	sudo apptainer instance start --env "GLANCES_OPT=-q --export influxdb2 --time 2" "${GLANCES_HOME}"/glances.sif glances "${INFLUXDB_HOST}" "${INFLUXDB_BUCKET}"
+	sudo apptainer instance start -B "${GLANCES_HOME}"/etc:/etc/glances --env "GLANCES_OPT=-q --export influxdb2 --time 2" glances.sif "${INFLUXDB_HOST}" "${INFLUXDB_BUCKET}"
 	sudo apptainer instance start "${RAPL_HOME}"/rapl.sif rapl "${INFLUXDB_HOST}" "${INFLUXDB_BUCKET}"
 fi
 
