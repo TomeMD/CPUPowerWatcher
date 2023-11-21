6e401ac25b9c630075b5b4be5e646736d1a2efc1
TomeMD <tome.maseda@udc.es>
2023-11-21 12:42:56 +0100

    Changed monitoring sampling frequency to 1s (to use InfluxDB window of 2s)

--- bin/init.sh
+++ bin/init.sh
@@ -2,10 +2,10 @@
 
 # Start monitoring environment
 if [ "${OS_VIRT}" == "docker" ]; then
-	docker run -d --name glances --pid host --privileged --network host --restart=unless-stopped -e GLANCES_OPT="-q --export influxdb2 --time 5" glances "${INFLUXDB_HOST}" "${INFLUXDB_BUCKET}"
+	docker run -d --name glances --pid host --privileged --network host --restart=unless-stopped -e GLANCES_OPT="-q --export influxdb2 --time 1" glances "${INFLUXDB_HOST}" "${INFLUXDB_BUCKET}"
 	docker run -d --name rapl --pid host --privileged --network host --restart=unless-stopped rapl "${INFLUXDB_HOST}" "${INFLUXDB_BUCKET}"
 else
-	sudo apptainer instance start -B "${GLANCES_HOME}"/etc:/etc/glances --env "GLANCES_OPT=-q --export influxdb2 --time 5" "${GLANCES_HOME}"/glances.sif glances "${INFLUXDB_HOST}" "${INFLUXDB_BUCKET}"
+	sudo apptainer instance start -B "${GLANCES_HOME}"/etc:/etc/glances --env "GLANCES_OPT=-q --export influxdb2 --time 1" "${GLANCES_HOME}"/glances.sif glances "${INFLUXDB_HOST}" "${INFLUXDB_BUCKET}"
 	sudo apptainer instance start "${RAPL_HOME}"/rapl.sif rapl "${INFLUXDB_HOST}" "${INFLUXDB_BUCKET}"
 fi
 
