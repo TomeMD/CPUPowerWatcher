9650cfc8ca418ddc16c668fe3a4da90670add872
TomeMD <tome.maseda@udc.es>
2024-08-12 17:33:17 +0200

    Separate physical and logical cores usage

--- tools/CPUCollector/cpumetrics/get-cpu-metrics.sh
+++ tools/CPUCollector/cpumetrics/get-cpu-metrics.sh
@@ -12,6 +12,27 @@ INFLUXDB_HOST=$2
 INFLUXDB_BUCKET=$3
 SAMPLING_FREQUENCY=1
 
+# Build a map of physical and logical cores
+declare -A LOG_PHY_MAPPING
+NUM_THREADS=$(lscpu | grep -e "^CPU(s):" | awk '{print $2}')
+
+# Array to register cores already found
+CORE_FOUND=()
+for (( i=0; i<NUM_THREADS; i++ )); do
+  CORE_FOUND[${i}]=0
+done
+
+# If we found a second CPU in the same core we mark it as a logical core
+OUTPUT=$(lscpu -e | awk 'NR > 1 { print $1, $4 }')
+while read -r CPU CORE; do
+  if [ "${CORE_FOUND[${CORE}]}" -eq "0" ];then
+    LOG_PHY_MAPPING[${CPU}]=1
+    CORE_FOUND[${CORE}]=1
+  else
+    LOG_PHY_MAPPING[${CPU}]=0
+  fi
+done <<< "$OUTPUT"
+
 function compute_core_utilization() {
   local CPU_TIMES=(${PROC_LINES[$(( CORE + 1 ))]})
   local TOTAL_TIME=0
@@ -58,6 +79,7 @@ done
 
 while true; do
     TOTAL_FREQ=0; TOTAL_TEMP=0; USER_UTIL=0; SYSTEM_UTIL=0; IOWAIT_UTIL=0
+    P_L_USER=(0 0); P_L_SYSTEM=(0 0)
     readarray -t PROC_LINES < /proc/stat
 
     DATA=""
@@ -70,6 +92,10 @@ while true; do
       SYSTEM_UTIL=$((SYSTEM_UTIL + SYSTEM_UTIL_CORE))
       IOWAIT_UTIL=$((IOWAIT_UTIL + IOWAIT_UTIL_CORE))
 
+      # Separate physical and logical usage
+      P_L_USER[${LOG_PHY_MAPPING[${CORE}]}]=$(( P_L_USER[${LOG_PHY_MAPPING[${CORE}]}] + USER_UTIL_CORE ))
+      P_L_SYSTEM[${LOG_PHY_MAPPING[${CORE}]}]=$(( P_L_SYSTEM[${LOG_PHY_MAPPING[${CORE}]}] + SYSTEM_UTIL_CORE ))
+
       # Add core data
       DATA+="cpu_metrics,core=${CORE} freq=${FREQ_CORE},user=${USER_UTIL_CORE},system=${SYSTEM_UTIL_CORE},iowait=${IOWAIT_UTIL_CORE} ${TIMESTAMP}\n"
     done
@@ -77,7 +103,7 @@ while true; do
     read_cpu_temperature
 
     # Add global data
-    DATA+="cpu_metrics,core=all avgfreq=${AVG_FREQ},sumfreq=${TOTAL_FREQ},user=${USER_UTIL},system=${SYSTEM_UTIL},iowait=${IOWAIT_UTIL},temp=${TOTAL_TEMP} ${TIMESTAMP}"
+    DATA+="cpu_metrics,core=all avgfreq=${AVG_FREQ},sumfreq=${TOTAL_FREQ},user=${USER_UTIL},puser=${P_L_USER[1]},luser=${P_L_USER[0]},system=${SYSTEM_UTIL},psystem=${P_L_SYSTEM[1]},lsystem=${P_L_SYSTEM[0]},iowait=${IOWAIT_UTIL},temp=${TOTAL_TEMP} ${TIMESTAMP}"
 
     # Send data to InfluxDB
     echo -e "${DATA}" | curl -s -XPOST "http://${INFLUXDB_HOST}:8086/api/v2/write?org=MyOrg&bucket=${INFLUXDB_BUCKET}" --header "Authorization: Token MyToken" --data-binary @-
